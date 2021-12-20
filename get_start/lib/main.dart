a4839108f9221bb74d18789ea61fffdba7028278
ShekAdnan <adnansk418@gmail.com>
2021-12-20 20:28:55 +0600

    first dart code

---
+++ get_start/lib/main.dart
@@ -0,0 +1,15 @@
+// In dart the entry point known as main funciton
+
+void main() {
+  //  main method is a functionn or a method in dart,  whatever  code we write within this main method willl be executed first
+
+  print(
+      'Hello World'); // we have to enclose text within the double quote open and double quote close
+  print('This is my first application');
+  print(6);
+  print(4 -
+      2); // we can also do some mathematical operation inside print statement
+  print(12 / 4);
+  print(true); // we can also print boolean value
+  print(false);
+}
